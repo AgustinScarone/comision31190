c0539f4f2ebc94b54d899f9801c56e85c4a1fb96
NitsugaS <agustin@momoagencia.com>
2022-05-13 17:26:52 -0300

    navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.scss'
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul class="navMenu">
+                <li><a href="https://www.google.com/">MENU</a></li>
+                <li><a href="https://www.google.com/">LOCALES</a></li>
+                <li><a href="https://www.google.com/">CONTACTO</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
