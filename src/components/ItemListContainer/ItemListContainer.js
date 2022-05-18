25b8833ac93cc0ac08c442accfe0e7ada8ea7a46
NitsugaS <agustin@momoagencia.com>
2022-05-18 02:57:03 -0300

    hover descripcion producto

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,10 +3,10 @@ import ItemProducts from "../ItemProducts/ItemProducts";
 const ItemListContainer = () => {
     return (
         <section className="sectionProducts">
-            <ItemProducts id={"burga01"} name={"CLASSIC BURGER"} stock={6}/>
-            <ItemProducts id={"burga02"} name={"EGG N BURGER"} stock={2}/>
-            <ItemProducts id={"burga03"} name={"CORONABURGER"} stock={6}/>
-            <ItemProducts id={"burga04"} name={"JIMMY KOMBO"} stock={3}/>
+            <ItemProducts menuImg={"burga01"} menuName={"HAMBURGUESA CLÁSICA"} menuData={"Fantástica hamburguesa con un medallón de carne 100% vacuna de 210g, queso cheddar, tomate, cebolla caramelizada con techo de crocante y fresca lechuga entre panes de la casa."}  menuPrice={"$799"} menuStock={3}/>
+            <ItemProducts menuImg={"burga02"} menuName={"HAMBURGUESA CON HUEVO"} menuData={"Hamburguesa con un medallón de carne 100% vacuna de 180g, tomate, lechuga, salsa especial, coronado con un regio huevo frito entre panes de la casa."}  menuPrice={"$799"} menuStock={6}/>
+            <ItemProducts menuImg={"burga03"} menuName={"CORONABURGER"} menuData={"360g de carne en dos medallones de carne 100% vacuna bañados en tormenta de queso emmental entre panes de la casa. Se recomienda realizar ejercicios de cardio una vez consumida para evitar problemas coronarios."}  menuPrice={"$999"} menuStock={5}/>
+            <ItemProducts menuImg={"burga04"} menuName={"JIMMY KOMBO"} menuData={"Hamburguesa clásica acompañada de crocantes papas fritas rústicas."}  menuPrice={"$899"} menuStock={2}/>
         </section>
     )
 }
--- src/components/ItemProducts/ItemProducts.js
+++ src/components/ItemProducts/ItemProducts.js
@@ -9,26 +9,33 @@ const ItemProducts = (props) =>{
     }
 
     const increment = () =>{
-        if(count < `${props.stock}`){
+        if(count < `${props.menuStock}`){
             setCount(count+1)
         }
     }
     return(
         <div className="cardProduct"
             style={{  
-                backgroundImage: `url(./img/products/${props.id}.jpg)`,
+                backgroundImage: `url(./img/products/${props.menuImg}.jpg)`,
                 backgroundPosition: 'center',
                 backgroundSize: 'cover',
                 backgroundRepeat: 'no-repeat'
             }}
             >
-            <h2>{props.name}</h2>   
-            <div className="contador">
-                <button onClick={decrement}>-</button> 
-                <div>{count}</div>
-                <button onClick={increment}>+</button>
+            <div className="containerMenu">
+                <img src="./img/angle-down-solid.svg" alt="+ Info" />
+                <h2>{props.menuName}</h2>
+                <article>{props.menuData}</article>
+                <div className="precio">{props.menuPrice}</div>
             </div>
-            <button>SUMAR AL CARRITO</button>
+            <div className="container">
+                <div className="contador">
+                    <button onClick={decrement}>-</button> 
+                    <div>{count}</div>
+                    <button onClick={increment}>+</button>
+                </div>
+                <button>SUMAR AL CARRITO</button>
+            </div>
         </div>
     )
 }
