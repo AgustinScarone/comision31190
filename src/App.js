6ee132fba6c4868e74d8df64e36311f55c1b9a95
Agustin Scarone <agustin@momoagencia.com>
2022-06-27 23:22:06 -0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,3 @@
-<<<<<<< HEAD
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-=======
 import './styles.css';
 
 import NavBar from './components/NavBar/NavBar';
@@ -61,7 +39,6 @@ const App = () => {
           <Footer />
         </BrowserRouter>
       </CartContextProvider>
->>>>>>> firebase02
     </div>
   );
 }
