79dc11b0c0bb874e7b49f2f60f5257ad48c856db
Agustin Scarone <agustin@momoagencia.com>
2022-06-27 23:49:48 -0300

    asd

--- src/components/Swiper/SwiperHome.js
+++ src/components/Swiper/SwiperHome.js
@@ -17,6 +17,7 @@ export default () => {
     <Swiper
         slidesPerView={1}
         spaceBetween={0}
+        initialSlide={1}
         loop={true}
         pagination={{
         clickable: true,
