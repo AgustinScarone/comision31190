e8d6d422dc4ea70b445284d22f3b8e70713b4152
Agustin Scarone <agustin@momoagencia.com>
2022-06-28 01:07:28 -0300

    firebase

--- src/services/firebase/index.js
+++ src/services/firebase/index.js
@@ -2,7 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 import 'firebase/firestore';
-import Axios from 'axios';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
@@ -28,20 +27,3 @@ export const collectionsName = {
   orders: 'orders',
   contact: 'contact'
 }
-
-// export const firebaseFunctions = require('firebase-functions')
-// export const nodemailer = require('nodemailer')
-// export const cors = require('cors')({
-//   origin: true
-// })
-
-// export const gmailEmail = functions.config().gmail.email
-// export const gmailPassword = functions.config().gmail.password
-
-// export mailTransport = nodemailer.createTransport({
-//   service: 'gmail',
-//   auth: {
-//     user: gmailEmail,
-//     pass: gmailPassword
-//   }
-// })
